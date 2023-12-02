9c4c88c62270d2087f1f28b29e5d4ad7a803ede8
EduardoBenitt <benitt.eduardo@gmail.com>
2023-12-02 18:40:13 -0400

    Consertado Recuperação de senha, agora está funcionando

--- app/src/main/java/com/example/prova_android_n2/MainActivity3.java
+++ app/src/main/java/com/example/prova_android_n2/MainActivity3.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.SignInMethodQueryResult;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -45,35 +44,16 @@ public class MainActivity3 extends AppCompatActivity {
                 }
 
                 // Log de depuração
-                Log.d("MainActivity3", "Verificando e-mail no Firebase...");
+                Log.d("MainActivity3", "Enviando e-mail de recuperação de senha...");
 
-                // Verificar se a conta com o e-mail informado existe no Firebase
-                mAuth.fetchSignInMethodsForEmail(email).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                // Enviar e-mail de recuperação de senha
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                    public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            SignInMethodQueryResult result = task.getResult();
-                            Log.d("MainActivity3", "Result: " + result.getSignInMethods());
-
-                            if (result.getSignInMethods().isEmpty()) {
-                                // Nenhuma conta com este e-mail encontrada
-                                Toast.makeText(MainActivity3.this, "Nenhuma conta encontrada com este e-mail.", Toast.LENGTH_SHORT).show();
-                            } else {
-                                // Conta encontrada, enviar e-mail de recuperação de senha
-                                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (task.isSuccessful()) {
-                                            Toast.makeText(MainActivity3.this, "E-mail de recuperação de senha enviado.", Toast.LENGTH_SHORT).show();
-                                        } else {
-                                            Toast.makeText(MainActivity3.this, "Erro ao enviar e-mail de recuperação de senha.", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
-                                });
-                            }
+                            Toast.makeText(MainActivity3.this, "E-mail de recuperação de senha enviado.", Toast.LENGTH_SHORT).show();
                         } else {
-                            // Erro ao verificar o e-mail no Firebase
-                            Toast.makeText(MainActivity3.this, "Erro ao verificar o e-mail.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity3.this, "Erro ao enviar e-mail de recuperação de senha.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
