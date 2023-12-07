44c89dd5323baf4fd11001d5ffd67e250e9c06be
EduardoBenitt <benitt.eduardo@gmail.com>
2023-12-06 22:04:58 -0400

    Criação do XML da pagina de favoritos

---
+++ app/src/main/java/com/example/prova_android_n2/MainActivity5.java
@@ -0,0 +1,18 @@
+package com.example.prova_android_n2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+
+
+
+
+    }
+}
